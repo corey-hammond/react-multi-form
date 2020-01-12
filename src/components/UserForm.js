7e4a3167d8c812bec2754174eee659cec4b2fe14
Corey Hammond <coreyjhammond@gmail.com>
2020-01-12 11:47:15 -0800

    setup Confirm component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import FormUserDetails from './FormUserDetails'
+import FormUserDetails from "./FormUserDetails";
+import FormPersonalDetails from "./FormPersonalDetails";
+import Confirm from "./Confirm";
 
 export class UserForm extends Component {
   state = {
@@ -48,9 +50,22 @@ export class UserForm extends Component {
           />
         );
       case 2:
-        return <h1>FormPersonalDetails</h1>;
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
       case 3:
-        return <h1>Confirm</h1>;
+        return (
+            <Confirm
+              nextStep={this.nextStep}
+              prevStep={this.prevStep}
+              values={values}
+            />
+          );
       case 4:
         return <h1>Success</h1>;
     }
